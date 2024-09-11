deb0388d60e90d8c91b3049009ae4759a43b24c4
Angel Hita <angel.hita@talentech.com>
2024-09-11 10:51:00 +0200

    migrating v16 ionic v8

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
-import { IonicStorageModule } from '@ionic/storage';
+import { IonicStorageModule } from '@ionic/storage-angular';
 
 import { TranslatePipe } from '@pipes/translate.pipe';
 
